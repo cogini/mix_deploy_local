f4114ba11948cfdbeb9104c2fef30acce6ec3830
Jake Morrison <jake@cogini.com>
2018-08-18 13:48:16 -0700

    Generate initial project

---
+++ lib/mix_deploy_local.ex
@@ -0,0 +1,18 @@
+defmodule MixDeployLocal do
+  @moduledoc """
+  Documentation for MixDeployLocal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixDeployLocal.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
